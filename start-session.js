b6f33cd27af79e4f588addd713353714746318ed
sVimon <yassinyass8788@gmail.com>
2025-12-23 22:09:24 +0100

    Update start-session.js

--- start-session.js
+++ start-session.js
@@ -17,6 +17,7 @@ document.addEventListener('DOMContentLoaded', function() {
         // يجب أن تكون كل ID بين علامات تنصيص ("") ومفصولة بفاصلة (,) عن الـ ID الذي يليه.
         const validIdsList = [ 
             "1502917719",
+            "1503044241",
             "1503001645",
             "1502976383",
             "1502972243",
@@ -139,6 +140,7 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 
 });
+
 
 
 
