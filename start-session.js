63c534981205449ead2d991377f5322d9a20e014
sVimon <yassinyass8788@gmail.com>
2025-12-25 07:07:32 +0100

    Update valid IDs in start-session.js
    Added new valid IDs to the list for user input validation.

--- start-session.js
+++ start-session.js
@@ -18,6 +18,7 @@ document.addEventListener('DOMContentLoaded', function() {
         const validIdsList = [ 
             "1502917719",
             "1503239125",
+            "1504451915",
             "1504113627",
             "1503124771",
             "1503848859",
@@ -153,6 +154,7 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 
 });
+
 
 
 
