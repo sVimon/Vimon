298b003ff065032488142143b437bcc7884aba08
sVimon <yassinyass8788@gmail.com>
2025-12-27 01:59:46 +0100

    Update start-session.js

--- start-session.js
+++ start-session.js
@@ -18,6 +18,7 @@ document.addEventListener('DOMContentLoaded', function() {
         const validIdsList = [ 
             "1502917719",
             "1506155031",
+            "1507037505",
             "1368201655",
             "1506893061",
             "1507013835",
@@ -196,6 +197,7 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 
 });
+
 
 
 
